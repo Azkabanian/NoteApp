8f0ed3403de0da0d2d9bfb0b44c38962e066f11a
ArturYusupov <YusupovArturrr@gmail.com>
2022-12-19 15:37:57 +0300

    init commit

---
+++ app/src/main/java/yusupov/noteapp/ui/theme/Color.kt
@@ -0,0 +1,11 @@
+package yusupov.noteapp.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val DarkGrey = Color(0xFF202020)
+val RedOrange = Color(0xffffab91)
+val LightBlue = Color(0xFFD7E8DE)
+val LightGreen = Color(0xffe7ed9b)
+val Violet  = Color(0xffcf94da)
+val BabyBlue  = Color(0xff81deea)
+val RedPink  = Color(0xfff48fb1)
---
+++ app/src/main/java/yusupov/noteapp/ui/theme/Theme.kt
@@ -0,0 +1,40 @@
+package yusupov.noteapp.ui.theme
+
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.darkColors
+import androidx.compose.material.lightColors
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+
+private val DarkColorPalette = darkColors(
+    primary = Color.White,
+    background = DarkGrey,
+    onBackground = Color.White,
+    surface = LightBlue,
+    onSurface = DarkGrey
+)
+
+private val LightColorPalette = lightColors(
+    primary = Color.White,
+    background = DarkGrey,
+    onBackground = Color.White,
+    surface = LightBlue,
+    onSurface = DarkGrey
+)
+
+@Composable
+fun NoteAppTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composable () -> Unit) {
+    val colors = if (darkTheme) {
+        DarkColorPalette
+    } else {
+        LightColorPalette
+    }
+
+    MaterialTheme(
+        colors = colors,
+        typography = Typography,
+        shapes = Shapes,
+        content = content
+    )
+}
